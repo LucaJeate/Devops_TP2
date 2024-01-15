f51fb24e41d236388429b248e8705b4b58d5220d
Lucas Legaz <lucas.legaz@gmail.com>
2024-01-15 17:03:07 +0100

    premier push

--- random-picking.py
+++ random-picking.py
@@ -1,18 +1,18 @@
 from math import comb
 
-def proba_tirage_uniforme(k,nb_noires,nb_blanches,nb_rouges):
-    total_boules=nb_noires+nb_blanches+nb_rouges
+def  proba_tirage_uniforme(k, nb_noires, nb_blanches, nb_rouges):
+    total_boules=nb_noires + nb_blanches + nb_rouges
 
     if k > total_boules:
         raise ValueError("Le nombre de boules tirées incohérent.")
 
-    comb_noires = comb(nb_noires,1)
-    comb_blanches = comb(nb_blanches,1)
-    comb_rouges = comb(nb_rouges,1)
+    comb_noires = comb(nb_noires, 1)
+    comb_blanches = comb(nb_blanches, 1)
+    comb_rouges = comb(nb_rouges, 1)
 
-    comb_total = comb(total_boules,k)
+    comb_total = comb(total_boules, k)
 
-    proba = (comb_noires*comb_blanches*comb_rouges)/comb_total
+    proba = (comb_noires * comb_blanches * comb_rouges) / comb_total
 
     return proba
 
@@ -21,5 +21,5 @@ nb_noires = 5
 nb_blanches = 4
 nb_rouges = 3
 
-probabilite = proba_tirage_uniforme(k,nb_noires,nb_blanches,nb_rouges)
+probabilite = proba_tirage_uniforme(k, nb_noires, nb_blanches, nb_rouges)
 print(f"La probabilité est : {probabilite:.4f}")
